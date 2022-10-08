45463d5b2b4a67115e6f0eb9e80c128df7aa0b52
Nidhin-mohan <nidhinmohannidhin>
2022-10-08 18:56:39 +0530

    Readme added

--- js/main.js
+++ js/main.js
@@ -5,16 +5,88 @@ let computerScoreDisplay = document.getElementById("compScoreVal");
 
 let userChoiceDisplay = document.getElementById("result-user-stat");
 let computerChoiceDisplay = document.getElementById("result-comp-stat");
-let result = document.getElementById("result-final-stat");
+let finalResult = document.getElementById("result-final-stat");
 
 let choices = document.querySelectorAll(".choice");
 let arr = ["rock", "paper", "scissors"];
+let userChoice;
+
+let userPoint = 0 ;
+let computerPoint = 0;
 
 choices.forEach(choice => choice.addEventListener("click", (e) => {
-    userChoiceDisplay.innerText =  "User: " + e.target.getAttribute("alt");
-  let test = () => {
-    arr[Math.floor(Math.random * arr.length)];
-  };
-    computerChoiceDisplay.innerText = test ;
+    userChoice = e.target.getAttribute("alt");
+    userChoiceDisplay.innerText =  "User: " + userChoice;
+ 
+    computerChoiceDisplay.innerText = "Computer: " + computerChoice();
+
+      resultcall();
+
 }))
 
+ let computerChoice = () => 
+   arr[Math.floor(Math.random() * arr.length)];
+ ;
+  // result check function
+
+function resultcall(){
+
+  console.log(userChoice + computerChoice());
+  check = userChoice + computerChoice();
+
+  if (
+    check == "rockscissors" ||
+    check == "scissorspaper "||
+    check == "paperrock"
+  ) {
+    finalResult.innerText = "You win";
+         userPoint++;
+         userScoreDisplay.innerText = userPoint;
+  }
+    if (
+      check == "scissorsrock" ||
+      check == "paperscissors" ||
+      check == "rockpaper"
+    ) {
+      finalResult.innerText = "computer win";
+           computerPoint++;
+           computerScoreDisplay.innerText = computerPoint;
+    }
+
+    else
+    {
+       finalResult.innerText = "Its a draw";
+    }
+
+
+
+
+    // switch (userChoice + computerChoice()) {
+    //   case "rockscissors":
+    //   case "scissorspaper":
+    //   case "paperrock":
+    //     finalResult.innerText = "You win";
+    //     userPoint++;
+    //     userScoreDisplay.innerText = userPoint;
+
+    //     break;
+    //   case "scissorsrock":
+    //   case "paperscissors":
+    //   case "rockpaper":
+    //     finalResult.innerText = "computer win";
+    //     computerPoint++;
+    //     computerScoreDisplay.innerText = computerPoint;
+    //     break;
+    //   case "paperpaper":
+    //   case "scissorsscissors":
+    //   case "rockrock":
+    //     finalResult.innerText = "Its a draw";
+    //     break;
+    //   default:
+    //     finalResult.innerText = "Its error";
+    // }
+   
+  
+ 
+
+}
